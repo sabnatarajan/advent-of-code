a5ae1887cba38cb9d45026a8f890039dba6924a1
Sab Natarajan <sab.natarajan@gmail.com>
2021-12-13 11:40:29 -0500

    feat(2021): Solved problem 2

--- 2021/02/p02.py
+++ 2021/02/p02.py
@@ -3,43 +3,52 @@
 from os import X_OK
 
 class Move:
-    def __init__(self, x=0, y=0):
-        self.x, self.y = x, y
+    def __init__(self, dir=0, mag=0):
+        self.dir, self.mag = dir, mag
 
     def __repr__(self) -> str:
-        return f"Move(x={self.x}, y={self.y})"
+        return f"Move(dir={self.dir}, mag={self.mag})"
+
 
 def part1(moves: list[Move]):
-    print(final_position(moves))
-
-def part2():
-    pass
-
-def final_position(moves: list[Move]):
     position = { "x": 0, "y": 0}
     for move in moves:
-        position['x'] += move.x
-        position['y'] += move.y
+        if move.dir == 'forward':
+            position['x'] += move.mag
+        elif move.dir == 'up':
+            position['y'] -= move.mag
+        elif move.dir == 'down':
+            position['y'] += move.mag
 
-    return position['x'] * position['y']
+    final_position = position['x'] * position['y']
+    print(final_position)
+
+def part2(moves: list[Move]):
+    aim = 0
+    position = { "x": 0, "y": 0 }
+    for move in moves:
+        if move.dir == 'down':
+            aim += move.mag
+        elif move.dir == 'up':
+            aim -= move.mag
+        elif move.dir == 'forward':
+            position['x'] += move.mag
+            position['y'] += move.mag * aim
+
+    final_position = position['x'] * position['y']
+    print(final_position)
 
 def read_input():
     moves = list()
     with open('input.txt', 'r') as f:
         for line in f.readlines():
-            splits = line.strip().split(' ')
-            direction = splits[0]
-            magnitude = int(splits[1].strip())
-            if direction == 'forward':
-                moves.append(Move(magnitude, 0))
-            elif direction == 'up':
-                moves.append(Move(0, -1*magnitude))
-            elif direction == 'down':
-                moves.append(Move(0, magnitude))
+            splits = line.strip().split()
+            move = Move(dir=splits[0], mag=int(splits[1]))
+            moves.append(move)
 
         return moves
 
 if __name__ == "__main__":
     inputs = read_input()
     part1(inputs)
-    part2()
+    part2(inputs)
